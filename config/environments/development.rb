a8295f7a3e8fc3680c592926838ddf80b1a413ed
EC2 Default User <ec2-user@ip-172-31-24-133.us-west-2.compute.internal>
2020-07-06 01:58:49 +0000

    fixbug login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  
+  #allowed ip
+  config.web_console.whitelisted_ips = '180.43.69.226'
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -29,6 +32,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  
+  #mailer setting
+  config.action_mailer.default_url_options = {host: 'localhost',port:3000}
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
